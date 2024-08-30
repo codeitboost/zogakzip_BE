c2e57393c5d104ff28bfc4d861e39846acee12d8
SjChO1125 <chosj8575@gmail.com>
2024-08-30 14:52:29 +0900

    Initial commit with project files

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const groupRoutes = require('./routes/groupRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// 데이터베이스 연결
+mongoose.connect('mongodb://localhost:27017/mydatabase')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// 미들웨어 설정
+app.use(bodyParser.json()); // 요청 본문을 JSON으로 파싱
+app.use('/api', groupRoutes); // '/api' 경로로 시작하는 모든 요청을 groupRoutes로 처리
+
+// 서버 시작
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
